80fadde976c8c6c6ca356015ac25d5f1ed4bf1ec
Josh <joshcoward63@yahoo.com>
2021-01-26 16:51:51 -0700

    data appended to csv and input fields are enforced

--- home.py
+++ home.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template,request, redirect
+from flask import Flask, render_template,request, redirect, request
 import webbrowser
 from threading import Timer
 # import cozmo_random_behaviors
+import json
+import os
+import csv
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,16 +18,37 @@ def startRandomBehavior():
     # cozmo_random_behaviors.start()
     return "hello"
 
-def refreshTime():
-    #Time is set to 0
-    pass
-
-def startTimer():
-    #Waits 5 minutes if no response from user refresh page.
-    pass
-
 def open_browser():
       webbrowser.open_new('http://127.0.0.1:8080/')
+
+@app.route('/enterDataInFile', methods=['POST'])
+def enterDataInFile():
+    description = request.form["description"]
+    data = request.form["form"]
+    data = data.split("&")
+    dataAsDict = {}
+    for key in data:
+        key = key.split("=")
+        dataAsDict[key[0]] = key[1]
+    interest_alarm = dataAsDict["likert2"]
+    confusion_understanding = dataAsDict["likert3"]
+    frusteration_relief = dataAsDict["likert4"]
+    sorrow_joy = dataAsDict["likert5"]
+    anger_gratitude = dataAsDict["likert6"]
+    fear_hope = dataAsDict["likert7"]
+    boredom_surprise = dataAsDict["likert8"]
+    disgust_desire = dataAsDict["likert9"]
+    try:
+        with open("data.csv", 'a') as file:
+            writer = csv.writer(file)
+            writer.writerows([description, interest_alarm, confusion_understanding, frusteration_relief, sorrow_joy, anger_gratitude, fear_hope, boredom_surprise, disgust_desire])
+    except IOError:
+        with open("data.csv", 'w') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Description", "Interst/Alarm", "Confusion/Understanding", "Frusteration/Relief","Sorrow/Joy", "Anger/Gratitude","Fear/Hope", "Boredom/Surprise","Disgust/Desire"])
+            writer.writerow([description, interest_alarm, confusion_understanding, frusteration_relief, sorrow_joy, anger_gratitude, fear_hope, boredom_surprise, disgust_desire])
+       
+
 
 if __name__ == '__main__':
     Timer(1, open_browser).start()
--- static/index.js
+++ static/index.js
@@ -1,7 +1,19 @@
+/*The Following are the buttons used in the html page*/
 const splash = document.querySelector('.splash');
 const start = document.getElementById("start");
 const startBot = document.querySelector('.startBot');
 const startBotButton = document.getElementById("startBotButton")
+const submitButton = document.getElementById("submitButton");
+/*The Following are the user description and */
+const descriptionField = document.getElementById("description");
+const interest_alarm = document.getElementsByName("likert2");
+const confusion_understanding = document.getElementsByName("likert3");
+const frusteration_relief = document.getElementsByName("likert4");
+const sorrow_joy = document.getElementsByName("likert5");
+const anger_gratitude = document.getElementsByName("likert6");
+const fear_hope = document.getElementsByName("likert7");
+const boredom_surprise = document.getElementsByName("likert8");
+const disgust_desire = document.getElementsByName("likert9");
 
 start.onclick = function(){
        setTimeout(()=>{
@@ -15,30 +27,24 @@ startBotButton.onclick = function(){
          startBot.classList.add('display-none');
  }, 2000); 
  console.log("startBotButton has been clicked!");
- }
-
- 
-const header = document.querySelector(".header");
-
-window.onscroll = function(){
-        var top = window.scrollY;
-        console.log(top);
-        if(top>=50){
-                header.classList.add('active');
-        }
-        else{
-                header.classList.remove("active");
-        }
 }
 
-// const startBot = document.getElementById("startBot");
+submitButton.onclick = function(){
 
-// startBot.onclick = function(){
-//         $.ajax({
-//                 type: "POST",
-//                 url: "/randomBehavior.py",
-//                 data: { param: text}
-//               }).done(function( o ) {
-//                  // do something
-//               });
+}
+ 
+
+
+
+// const header = document.querySelector(".header");
+
+// window.onscroll = function(){
+//         var top = window.scrollY;
+//         console.log(top);
+//         if(top>=50){
+//                 header.classList.add('active');
+//         }
+//         else{
+//                 header.classList.remove("active");
+//         }
 // }
